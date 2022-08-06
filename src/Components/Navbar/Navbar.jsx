64dc085dedce18a1963bed40e7548d489e93bc23
Gabriel Monge <gabriel.monge.lizano@gmail.com>
2022-08-05 20:07:17 -0600

    Idk

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import "./navbar.css";
 export default function Navbar() {
   return (
     <section className="top-nav">
-      <div><a href="/">Gabriel Monge</a></div>
+      <div><a href="/home">Gabriel Monge</a></div>
       <input id="menu-toggle" type="checkbox" />
       <label className="menu-button-container" htmlFor="menu-toggle">
         <div className="menu-button"></div>
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
